469a40d6deae2281487dbbcf1205c710478fbadf
Anthony Giles <omgliekpie@gmail.com>
2020-09-01 15:18:19 -0600

    write table of contents program

---
+++ learn_to_program/ch5/contents.rb
@@ -0,0 +1,5 @@
+lineWidth = 80
+puts "Table of Contents\n".center lineWidth
+puts "Chapter 1: Numbers".ljust(lineWidth/2) + "page 1\n".rjust(lineWidth/2)
+puts "Chapter 2: Letters".ljust(lineWidth/2) + "page 72\n".rjust(lineWidth/2)
+puts "Chapter 3: Variables".ljust(lineWidth/2) + "page 118\n".rjust(lineWidth/2)
