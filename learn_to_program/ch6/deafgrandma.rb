a4162b5b14243e270d71384c367db3775d01a386
Anthony Giles <omgliekpie@gmail.com>
2020-09-02 11:26:26 -0600

    have to say BYE 3 times to exit

--- learn_to_program/ch6/deafgrandma.rb
+++ learn_to_program/ch6/deafgrandma.rb
@@ -1,14 +1,25 @@
 puts "What would you like to say to Granny?"
 str = gets.chomp
-while str != 'BYE'
-    if str == str.upcase
+bye_count = 0
+while (bye_count < 3)
+    if (str == str.upcase and str != 'BYE')
+        bye_count = 0
         puts "NO, NOT SINCE #{rand(1930..1950)}!"
         puts "What would you like to say to Granny?"
         str = gets.chomp
-    else
+    elsif (str != str.upcase and str != 'BYE')
+        bye_count = 0
         puts "HUH?! SPEAK UP, SONNY!"
         puts "What would you like to say to Granny?"
         str = gets.chomp
+    elsif str == 'BYE'
+        bye_count = bye_count + 1
+        if bye_count == 3
+            puts 'BYE SONNY!'
+        else
+        puts 'YOU GOT WHAT IN YOUR EYE?!?'
+        puts "What would you like to say to Granny?"
+        str = gets.chomp
+        end      
     end
 end
-puts "YOU GOT WHAT IN YOUR EYE?!"
