d908661803a6080c48ad3fb968bc9e2a1e112484
Anthony Giles <omgliekpie@gmail.com>
2020-09-25 09:58:01 -0600

    continue work on stock_picker method

--- top/basic-ruby-projects/stock_picker.rb
+++ top/basic-ruby-projects/stock_picker.rb
@@ -14,27 +14,50 @@ example:
 
 =end
 
+require 'pry'
+
 def stock_picker(prices)
+  # Use an array to store the data we find with each element being another
+  # array with the following data [max_profit, buy_day, sell_day]
+  max_profit_per_day = []
+
   # Iterate over 'prices' array and for each buy day calculate the maximum
   # possible profit if you bought on that day.
   prices.each_with_index do |buy_price, buy_day|
+
+    # test values
     puts "Buy Day: #{buy_day} Buy Price: #{buy_price}"
 
     # Get all the remaining days after the current buy_day.
     remaining = prices[buy_day + 1..-1]
-    
+
+    # Use an array to store the data for the current day
+    possible_profit = []
+
     # Iterate over all the remaining days and calculate the profit if you
     # were to sell on that day.
     remaining.each_with_index do |sell_price, sell_day|
+
+      # test values
       puts "Profit: #{sell_price - buy_price} Sell Day: #{sell_day + buy_day + 1}"
 
       # format the data imagined in the above puts and store the data in an
       # array
+      possible_profit.push([sell_price - buy_price, buy_day, sell_day + buy_day + 1])
     end
+    
+    # Sort the possible profits for the day and push the highest one to the
+    # max_profit_per_day array.
+    max_profit_per_day[buy_day] = possible_profit.sort.pop
+
   end
+
+# Get rid of the nil value, and sort the array.
+max_profit_per_day.compact.sort.pop
+
 end
 
-puts stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([17,3,6,9,15,8,6,1,10])
 
 =begin
 Idea 1:
