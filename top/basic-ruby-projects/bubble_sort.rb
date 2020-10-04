20bbd4cbdd43ffdbda825a628e4df2e003d600c4
Anthony Giles <omgliekpie@gmail.com>
2020-10-04 11:48:36 -0600

    lint code

--- top/basic-ruby-projects/bubble_sort.rb
+++ top/basic-ruby-projects/bubble_sort.rb
@@ -1,43 +1,39 @@
-=begin
-Algorithm:
-For each element in list look at element and element directly to the right.
-If out of order, swap elements.
-=end
+# frozen_string_literal: true
+
+# Algorithm:
+# For each element in list look at element and element directly to the right.
+# If out of order, swap elements.
 
 def bubble_sort(unsorted_numbers)
-
   # Make an array to hold the highest number as it bubbles up.
   sorted_numbers = []
 
   # As long as there are numbers to sort, keep sorting.
-  while unsorted_numbers.length > 0 do
+  while unsorted_numbers.length.positive?
 
     # Use a variable to track the current starting index.
     start_index = 0
 
-      # Keep looping through the array until we are on the last pair
-      while start_index < unsorted_numbers.length - 1 do
+    # Keep looping through the array until we are on the last pair
+    while start_index < unsorted_numbers.length - 1
 
-        # If we get down to the last item break out of the loop.
-        if unsorted_numbers.length == 1
-          break
+      # If we get down to the last item break out of the loop.
+      break if unsorted_numbers.length == 1
 
-        end
+      # Select a pair of numbers
+      unsorted_pair = select_pair(unsorted_numbers, start_index)
 
-        # Select a pair of numbers 
-        unsorted_pair = select_pair(unsorted_numbers, start_index)
+      # Look at the pair of numbers and determine their correct order.
+      sorted_pair = sort_pair(unsorted_pair[0], unsorted_pair[1])
 
-        # Look at the pair of numbers and determine their correct order.
-        sorted_pair = sort_pair(unsorted_pair[0], unsorted_pair[1])
+      # Reassign the array elements to correspond with correct order.
+      unsorted_numbers[start_index] = sorted_pair[0]
+      unsorted_numbers[start_index + 1] = sorted_pair[1]
 
-        # Reassign the array elements to correspond with correct order.
-        unsorted_numbers[start_index] = sorted_pair[0]
-        unsorted_numbers[start_index + 1] = sorted_pair[1]
+      # Increment the start_index
+      start_index += 1
 
-        # Increment the start_index
-        start_index += 1
-
-      end
+    end
 
     # After the whole thing has gone through once and you have the highest
     # number pop the element out and add it to the front of sorted_numbers.
@@ -47,7 +43,6 @@ def bubble_sort(unsorted_numbers)
 
   # When done return the sorted numbers.
   sorted_numbers
-
 end
 
 def sort_pair(first_number, second_number)
@@ -55,7 +50,7 @@ def sort_pair(first_number, second_number)
 end
 
 def select_pair(array_of_numbers, start_index)
-  pair = [array_of_numbers[start_index], array_of_numbers[start_index + 1]]
+  [array_of_numbers[start_index], array_of_numbers[start_index + 1]]
 end
 
-p bubble_sort([4,3,78,2,0,2])
+p bubble_sort([4, 3, 78, 2, 0, 2])
