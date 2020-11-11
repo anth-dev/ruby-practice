50752c00cce1cad6a26da22e1fab9a12e30dd200
Anthony Giles <omgliekpie@gmail.com>
2020-11-11 15:54:45 -0700

    remove pry requirement

--- top/recursion/codequiz_flatten_array.rb
+++ top/recursion/codequiz_flatten_array.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'pry'
-
 # Define a recursive function that flattens an array. The method should convert
 # [[1, 2], [3, 4]] to [1, 2, 3, 4] and [[1, [8, 9]], [3, 4]] to [1, 8, 9, 3, 4].
 
