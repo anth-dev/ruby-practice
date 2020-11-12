5cbc55c6b4176c60452c0a0ca8618f231a01da12
Anthony Giles <omgliekpie@gmail.com>
2020-11-11 17:52:44 -0700

    fill in provided information and requirements

---
+++ top/recursion/codequiz_int_to_roman_numeral.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# Use the roman_mapping hash to define a recursive method that converts an
+# integer to a Roman numeral.
+
+def to_roman_numeral(int, conversion_map)
+
+end
+
+roman_mapping = {
+  1000 => "M",
+  900 => "CM",
+  500 => "D",
+  400 => "CD",
+  100 => "C",
+  90 => "XC",
+  50 => "L",
+  40 => "XL",
+  10 => "X",
+  9 => "IX",
+  5 => "V",
+  4 => "IV",
+  1 => "I"
+}
+
+puts to_roman_numeral(35, roman_mapping)
