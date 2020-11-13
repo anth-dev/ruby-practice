17903d454ee3342c392d4087bf85b9f1e368c17f
Anthony Giles <omgliekpie@gmail.com>
2020-11-12 22:36:19 -0700

    complete question

--- top/recursion/codequiz_roman_numeral_to_int.rb
+++ top/recursion/codequiz_roman_numeral_to_int.rb
@@ -6,34 +6,34 @@
 def roman_numeral_to_int(roman_numeral, conversion_map, integer = 0)
   return integer if conversion_map.empty?
 
-  # Shift out the current conversion information.
   current_conversion = conversion_map.shift
 
-  # Store the numeral and integer conversion values in their own variables.
   conversion_numeral = current_conversion.first
   conversion_integer = current_conversion.last
 
-  # Check how many times the conversion numeral occurs at the start of the
-  #   roman_numeral string and add the numeral's value to integer. Also remove
-  #   the converted numerals from roman_numeral.
+  while roman_numeral[0, conversion_numeral.length] == conversion_numeral
+    integer += conversion_integer
+
+    roman_numeral = roman_numeral.sub(conversion_numeral, '')
+  end
 
   roman_numeral_to_int(roman_numeral, conversion_map, integer)
 end
 
 roman_mapping = {
-  "M" => 1000,
-  "CM" => 900,
-  "D" => 500,
-  "CD" => 400,
-  "C" => 100,
-  "XC" => 90,
-  "L" => 50,
-  "XL" => 40,
-  "X" => 10,
-  "IX" => 9,
-  "V" => 5,
-  "IV" => 4,
-  "I" => 1
+  'M' => 1000,
+  'CM' => 900,
+  'D' => 500,
+  'CD' => 400,
+  'C' => 100,
+  'XC' => 90,
+  'L' => 50,
+  'XL' => 40,
+  'X' => 10,
+  'IX' => 9,
+  'V' => 5,
+  'IV' => 4,
+  'I' => 1
 }
 
 puts roman_numeral_to_int('MCMLXXXIV', roman_mapping)
