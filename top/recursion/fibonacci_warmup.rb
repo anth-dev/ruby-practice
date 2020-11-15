f8517107f8bf687e0a985146a6b263c91ad7ad39
Anthony Giles <omgliekpie@gmail.com>
2020-11-15 13:48:14 -0700

    complete assignment 1

--- top/recursion/fibonacci_warmup.rb
+++ top/recursion/fibonacci_warmup.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'pry'
+
 # Write a method #fibs which takes a number and returns that many members of
 #   the fibonacci sequence. Use iteration for this solution.
 def fibs(n)
@@ -32,3 +34,20 @@ p fibs(10)
 # Now write another method #fibs_rec which solves the same problem recursively.
 #   This can be done in just 3 lines (or 1 if you’re crazy, but don’t consider
 #   either of these lengths a requirement… just get it done).
+def fibs_rec(n, current_n = 0, fibonacci_sequence = [], a = 0, b = 1)
+  if current_n.zero?
+    fibonacci_sequence.push(0)
+  elsif current_n == 1
+    fibonacci_sequence.push(1)
+  else
+    sum = a + b
+    fibonacci_sequence.push(sum)
+    a = b
+    b = sum
+  end
+  
+  return fibonacci_sequence if n == current_n
+  fibs_rec(n, current_n + 1, fibonacci_sequence, a, b)
+end
+
+p fibs_rec(10)
