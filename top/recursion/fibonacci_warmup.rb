280b50afec4efc69f651b5805fae12feda5b8740
Anthony Giles <omgliekpie@gmail.com>
2020-11-15 09:34:31 -0700

    remove pseudocode comments

--- top/recursion/fibonacci_warmup.rb
+++ top/recursion/fibonacci_warmup.rb
@@ -3,8 +3,6 @@
 # Write a method #fibs which takes a number and returns that many members of
 #   the fibonacci sequence. Use iteration for this solution.
 def fibs(n)
-  # Use variables to store the current n and values for a, b, and an array to
-  #   eventually return.
   current_n = 2
   a = 0
   b = 1
@@ -16,23 +14,16 @@ def fibs(n)
   fibonacci_sequence.push(b)
   return fibonacci_sequence if n == 1
 
-  # While the current n doesn't equal n, calculate fibonacci numbers pushing
-  #   them to an array that will be returned at the end of the method.
   until current_n > n
-    # Calculate fibonacci numbers and push them to the fibonacci_sequence
-    #   array.
     sum = a + b
     fibonacci_sequence.push(sum)
 
-    # Prepare variables for the next iteration of the loop.
     a = b
     b = sum
 
-    # Increment current_n.
     current_n += 1
   end
 
-  # Return the array of fibonacci numbers when the while loop has completed.
   fibonacci_sequence
 end
 
