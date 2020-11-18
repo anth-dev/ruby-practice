c53b67e6992d41a376dff75d6e14e0b53fb6e01e
Anthony Giles <omgliekpie@gmail.com>
2020-11-17 20:26:07 -0700

    complete merge_sort method

--- top/a-bit-of-cs-projects/merge_sort.rb
+++ top/a-bit-of-cs-projects/merge_sort.rb
@@ -1,14 +1,21 @@
 # frozen_string_literal: true
 
 def merge_sort(unsorted_array)
-  # If unsorted_array contains more than one item, split it in half and call
-  #   merge_sort on each of the two halfs.
+  if unsorted_array.length > 1
+    left_side = unsorted_array.take(unsorted_array.length / 2)
+    right_side = unsorted_array.drop(unsorted_array.length / 2)
 
-  # When the two halfs are returned sorted, merge them together in order.
+    sorted_left_side = merge_sort(left_side)
+    sorted_right_side = merge_sort(right_side)
 
-  # If unsorted_array is just one item consider it sorted and return it back to
-  #   the method that called it.
-  unsorted_array
+    sorted_array = []
+    until sorted_left_side.empty? || sorted_right_side.empty?
+      sorted_left_side.first < sorted_right_side.first ? sorted_array.push(sorted_left_side.shift) : sorted_array.push(sorted_right_side.shift)
+    end
+    sorted_array.concat(sorted_left_side, sorted_right_side)
+  else
+    unsorted_array
+  end
 end
 
 p merge_sort([10, 2, 9, 4, 1, 6, 8, 5, 7, 3])
