e55bd359537de85b0dc0ae6ab78b1f1683c8a3ac
Anthony Giles <omgliekpie@gmail.com>
2020-12-07 15:29:41 -0700

    Create Node class and make it's objects comparable.

--- top/a-bit-of-cs-projects/balanced_binary_search.rb
+++ top/a-bit-of-cs-projects/balanced_binary_search.rb
@@ -1,2 +1,25 @@
 # frozen_string_literal: true
 
+class Node
+  include Comparable
+  attr :data
+
+  def initialize(data)
+    @data = data
+    @left_child = nil
+    @right_child = nil
+  end
+
+  def <=>(other_Node)
+    data <=> other_Node.data
+  end
+end
+
+first_node = Node.new(3)
+second_node = Node.new(5)
+
+p first_node
+p second_node
+
+p first_node < second_node
+p first_node > second_node
