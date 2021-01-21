1635f6a940a89b42ba245ec32ec0270b85e1efe9
Anthony Giles <omgliekpie@gmail.com>
2021-01-20 22:09:41 -0700

    Add pretty_print to Tree class and begin working on build_tree method.

--- top/a-bit-of-cs-projects/balanced_binary_search.rb
+++ top/a-bit-of-cs-projects/balanced_binary_search.rb
@@ -27,8 +27,12 @@ class Tree
   # The build_tree method should return the level-1 root node.
   def build_tree(array)
     # Set the base case to stop recursion.
+    # return nil if ...
 
     # Make sure the array is sorted and has no duplicates values.
+    prepared_array = array.sort.uniq
+
+    binding.pry
 
     # Find the middle of the array.
 
@@ -38,15 +42,21 @@ class Tree
 
     # Recursively cass build_tree method on all elements right of middle.
   end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 end
 
+# Testing comparable module.
 first_node = Node.new(3)
 second_node = Node.new(5)
-
-p first_node
-p second_node
-
 p first_node < second_node
 p first_node > second_node
 
-test_tree_class = Tree.new([1])
+# Testing Tree class.
+test_tree_one = Tree.new([2, 1, 3])
+
+pp test_tree_one
