8e267b3186887a346e26e280838242cb83be459b
Anthony Giles <omgliekpie@gmail.com>
2021-02-03 20:10:07 -0700

    Start on level_order method.

--- top/a-bit-of-cs-projects/balanced_binary_search.rb
+++ top/a-bit-of-cs-projects/balanced_binary_search.rb
@@ -171,6 +171,10 @@ class Tree
 
   end
 
+  def level_order(node)
+
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
