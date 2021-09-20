951800011ea000ee5b81a55d1ba44f5bc8d7ffae
Anthony Giles <omgliekpie@gmail.com>
2021-09-20 17:16:59 -0600

    Implement some of rubocops suggestions

--- top/custom_enumerables/enumerables.rb
+++ top/custom_enumerables/enumerables.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
+
 require 'pry'
 
+# These are recreations of some of Ruby's built in enumerable methods.
 module Enumerable
   def my_each
     return Enumerator.new(self) unless block_given?
 
-    copy_of_array = self.dup
+    copy_of_array = dup
     i = 0
-    until i == self.length do
+    until i == length
       yield copy_of_array.shift
       i += 1
     end
@@ -17,9 +19,9 @@ module Enumerable
   def my_each_with_index
     return Enumerator.new(self) unless block_given?
 
-    copy_of_array = self.dup
+    copy_of_array = dup
     i = 0
-    until i == self.length do
+    until i == length
       yield(copy_of_array.shift, i)
       i += 1
     end
@@ -30,8 +32,7 @@ module Enumerable
     return Enumerator.new(self) unless block_given?
 
     array_of_truth = []
-    # copy_of_array = self.dup
-    self.my_each { |element| array_of_truth << element if yield(element) }
+    my_each { |element| array_of_truth << element if yield(element) }
 
     array_of_truth
   end
@@ -41,25 +42,25 @@ numbers = [1, 2, 3, 4, 5]
 
 # Script for checking my_each
 
-puts "my_each - puts each item"
+puts 'my_each - puts each item'
 numbers.my_each { |item| puts item }
 
-puts "each - puts each item"
+puts 'each - puts each item'
 numbers.each { |item| puts item }
 
-puts "my_each_return_value with_block"
-my_each_return_value = numbers.my_each { |x| 'I am in a block!' }
+puts 'my_each_return_value with_block'
+my_each_return_value = numbers.my_each { 'I am in a block!' }
 p my_each_return_value
 
-puts "each return value with block"
-each_return_value = numbers.each { |x| 'I am also in a block!' }
+puts 'each return value with block'
+each_return_value = numbers.each { 'I am also in a block!' }
 p each_return_value
 
-puts "my_each return value without block"
+puts 'my_each return value without block'
 my_each_return_value = numbers.my_each
 p my_each_return_value
 
-puts "each return value without block"
+puts 'each return value without block'
 each_return_value = numbers.each
 p each_return_value
 
@@ -71,19 +72,19 @@ numbers.each_with_index { |item, index| puts "#{index}: #{item}" }
 puts "my_each_with_index - puts each item and it's index"
 numbers.my_each_with_index { |item, index| puts "#{index}: #{item}" }
 
-puts "each_with_index return value with block"
-each_with_index_return_value = numbers.each_with_index { |item, index| 'I am in the block'}
+puts 'each_with_index return value with block'
+each_with_index_return_value = numbers.each_with_index { 'I am in the block'}
 p each_with_index_return_value
 
-puts "my_each_with_index return value with block"
-my_each_with_index_return_value = numbers.my_each_with_index { |item, index| 'I am in the block'}
+puts 'my_each_with_index return value with block'
+my_each_with_index_return_value = numbers.my_each_with_index { 'I am in the block'}
 p my_each_with_index_return_value
 
-puts "each_with_index return value without block"
+puts 'each_with_index return value without block'
 each_with_index_return_value = numbers.each_with_index
 p each_with_index_return_value
 
-puts "my_each_with_index return value without block"
+puts 'my_each_with_index return value without block'
 my_each_with_index_return_value = numbers.my_each_with_index
 p my_each_with_index_return_value
 
